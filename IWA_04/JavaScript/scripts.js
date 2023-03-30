1dfd6ab8964e54e805eccaff2ea27867027879fd
TaraKoala <75086178+TaraKoala@users.noreply.github.com>
2023-03-30 16:03:03 +0200

    updates to challenge 1 in IWA_04

--- IWA_04/JavaScript/scripts.js
+++ IWA_04/JavaScript/scripts.js
@@ -1,33 +1,34 @@
-const date = 2050
+const year = 2050
 let status = 'student'
 let count = 0
 
- if (date = 2050) {
+
+ if (year === 2050) {
  	console.log("January", 'New Year’s Day')
  	console.log("March", 'Human Rights Day')
  	date = 'April'
  	console.log(date, 'Family Day')
  	console.log(date, 'Freedom Day')
- 	let count = count + 4
+ 	let count = count + 4;
 	
-	if (status = "student") {
- 	  console.log('June', 'Youth Day')
- 		let count = count + 1
+	if (console.log('June', 'Youth Day')) {
+		let status = student
+ 		let count = count + 1;
  	}
 
 	console.log('August', 'Women’s Day')
 	console.log('September', 'Heritage Day')
 	date = 'December'
 	console.log(date, 'Day of Reconciliation')
-	let count = count + 3
+	let count = count + 3;
 
-	if (status = "parent") {
-	  console.log(date, 'Christmas Day')
-		let count = count + 1
+	if (console.log(date, 'Christmas Day')) {
+		let status = 'parent'
+		let count = count + 1;
 	}
 
 	console.log(date, 'Day of Goodwill')
-	let count = count + 1
+	let count = count + 1;
 }
 
 console.log('Your status is:', status)
