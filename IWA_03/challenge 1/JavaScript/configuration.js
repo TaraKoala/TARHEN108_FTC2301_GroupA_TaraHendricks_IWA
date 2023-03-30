a7ea5fb12faefef1c40d2135e6000ace358ecb87
TaraKoala <75086178+TaraKoala@users.noreply.github.com>
2023-03-30 11:14:23 +0200

    changed import to export on configuration script

--- IWA_03/challenge 1/JavaScript/configuration.js
+++ IWA_03/challenge 1/JavaScript/configuration.js
@@ -1,9 +1,9 @@
 //This is the company name
-const company = 'ACME Inc.'
+export const company = 'ACME Inc.'
 
 
 //This is the established year
-const year = '2022'
+export const year = '2022'
 
 
 console.log(company, year)
