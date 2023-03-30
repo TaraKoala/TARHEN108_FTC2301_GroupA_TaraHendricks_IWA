5289bd4953794a66a67773bc07476930dff35b5c
TaraKoala <75086178+TaraKoala@users.noreply.github.com>
2023-03-30 14:52:44 +0200

    updated code on challenge 1 for IWA_04

--- IWA_04/JavaScript/scripts.js
+++ IWA_04/JavaScript/scripts.js
@@ -1,19 +1,19 @@
-const date: 2050
-const status: 'student'
-const count = 0
+const date = 2050
+let status = 'student'
+let count = 0
 
-if (date = 2050) {
-	console.log("January", 'New Year’s Day')
-	console.log("March", 'Human Rights Day')
-	date = 'April'
-	console.log(date, 'Family Day')
-	console.log(date, 'Freedom Day')
-	let count = count + 4
-
+ if (date = 2050) {
+ 	console.log("January", 'New Year’s Day')
+ 	console.log("March", 'Human Rights Day')
+ 	date = 'April'
+ 	console.log(date, 'Family Day')
+ 	console.log(date, 'Freedom Day')
+ 	let count = count + 4
+	
 	if (status = "student") {
-	  console.log('June', 'Youth Day')
-		let count = count + 1
-  }
+ 	  console.log('June', 'Youth Day')
+ 		let count = count + 1
+ 	}
 
 	console.log('August', 'Women’s Day')
 	console.log('September', 'Heritage Day')
@@ -24,7 +24,7 @@ if (date = 2050) {
 	if (status = "parent") {
 	  console.log(date, 'Christmas Day')
 		let count = count + 1
-  }
+	}
 
 	console.log(date, 'Day of Goodwill')
 	let count = count + 1
