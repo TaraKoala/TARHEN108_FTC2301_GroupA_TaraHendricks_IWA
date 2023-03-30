22bd790cc6bc4ea652157b6889a8df5019bcf2b4
TaraKoala <75086178+TaraKoala@users.noreply.github.com>
2023-03-30 10:36:37 +0200

    updated challenge 1 code to fix error message

--- IWA_03/challenge 1/JavaScript/script.js
+++ IWA_03/challenge 1/JavaScript/script.js
@@ -1,7 +1,8 @@
 // scripts.js
-
-import company form 'configuration'
-import year form 'configuration'
+import {company} from './configuration.js'
+import {year} from './configuration.js'
 
 const message = '© ' + company + ' (' + year + ')'
 document.querySelector('footer').innerText = message
+
+console.log(company, year)
