be5969052a2c5e111dcc4312c35264eac78d85cb
TaraKoala <75086178+TaraKoala@users.noreply.github.com>
2023-04-04 11:01:31 +0200

    updated code in IWA_05

--- IWA_05/JavaScript/scripts.js
+++ IWA_05/JavaScript/scripts.js
@@ -1,21 +1,24 @@
-FREE_WARNING = 'Free shipping only applies to single customer orders'
-BANNED_WARNIN = 'Unfortunately we do not ship to your country of residence'
-NONE_SELECTED = '0'
+const FREE_WARNING = 'Free shipping only applies to single customer orders';
+const BANNED_WARNING = 'Unfortunately we do not ship to your country of residence';
+const NONE_SELECTED = '0';
 
-if (location = RSA) { shipping === 400 && currency === 'R' }
+if (location = RSA) { shipping === 400 && currency === 'R' 
 
-if location = NAM
+if (location = NAM) {
 shipping = 600 
-else shipping = 800
+else {shipping = 800
+}
 
-shoes = 300 * 1
-toys - 100 * 5
-shirts = 150 * 'NONE_SELECTED'
-batteries 35 * 2
-pens = 5 * 'NONE_SELECTED' 
+let shoes = 300 * 1
+let toys - 100 * 5
+let shirts = 150 * 'NONE_SELECTED'
+let batteries 35 * 2
+let pens = 5 * 'NONE_SELECTED' 
 
 shipping = null
 currency = $
+}
+}
 
 if (shoes + batteries + pens + shirts > 1000 &&  ) {
 	if (location = NAM && customers < 2) {
@@ -23,11 +26,11 @@ if (shoes + batteries + pens + shirts > 1000 &&  ) {
 		    shipping = 0 || calcShipping
 		}
 	}
-}
 
-if (shipping = 0) && (customers !=== 1) { console.log(WARNING) }
 
-location = 'NK' ? console.log(WARNING) : console.log('price', currency, shoes + batteries + pens + shirts + shipping)
+if (shipping = 0) && (customers !=== 1) { console.log(FREE_WARNING) }
+
+location = 'NK' ? console.log(BANNED_WARNING) : console.log('price', currency, shoes + batteries + pens + shirts + shipping)
 
 customers = '1'
 const location = 'RSA'
