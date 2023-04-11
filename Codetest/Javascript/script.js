d0d08d567254ed69478348db6648022a488e1394
TaraKoala <75086178+TaraKoala@users.noreply.github.com>
2023-04-11 09:50:54 +0200

    Updated indexes and added a folder for test code

---
+++ Codetest/Javascript/script.js
@@ -0,0 +1,25 @@
+console.log('test')
+
+if(1 === 1) { console.log('test')}
+
+if (1 === 2) { console.log('test')}
+
+if (1 === 2) { console.log('test')} else {
+    console.log('not true')
+}
+// let value;
+
+// if (1 === 2) {
+//     value = 'a'
+// } else {
+//     value = 'b'
+// }
+
+// console.log(value)
+
+let value = (1 === 2 ? 'a' : 'b')
+
+console.log(value)
+
+const result = ( 1 === 1 && 5 === 4)
+console.log(result)
