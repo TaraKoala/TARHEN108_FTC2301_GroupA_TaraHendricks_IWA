5cdb129fba9d301d7fa9fe3263ed81d2559e1597
TaraKoala <75086178+TaraKoala@users.noreply.github.com>
2023-05-02 02:53:51 +0200

    updated code in challenges

--- IWA_13/JavaScript/script.js
+++ IWA_13/JavaScript/script.js
@@ -5,24 +5,44 @@ let calculated = '1'
 // Only allowed to change below
 
 
-const = logCalc () => { 
-    const isString = typeof calculated = 'numerical-string' 
-    const calculatedAsNumber = isString ? calculated : parseNumber(calculated)
-    calculated === calculatedAsNumber + 1 
+// const = logCalc () => { 
+//     const isString = typeof calculated = 'numerical-string' 
+//     const calculatedAsNumber = isString ? calculated : parseNumber(calculated)
+//     calculated === calculatedAsNumber + 1 
+// }
+
+// const = calcUser () => {
+//   logCalc
+//   if (calculated > 2) user = 'John'
+//   if (calculated > 2) state = 'requesting'
+//   if (calculated > 3) state = 'idle'
+// }
+
+// const = checkUser () => {
+// 	if (user && state === 'requesting') {
+// 		console.log(`User: ${user} (${calculated})`)
+// 	}
+// }
+
+const logCalc = () => { 
+  const isString = typeof calculated === 'string' 
+  const calculatedAsNumber = isString ? parseInt(calculated) : calculated
+  calculated = calculatedAsNumber + 1 
 }
 
-const = calcUser () => {
-  logCalc
-  if (calculated > 2) user = 'John'
-  if (calculated > 2) state = 'requesting'
-  if (calculated > 3) state = 'idle'
+const calcUser = () => {
+logCalc()
+if (calculated > 2) user = 'John'
+if (calculated > 2) state = 'requesting'
+if (calculated > 3) state = 'idle'
 }
 
-const = checkUser () => {
-	if (user && state === 'requesting') {
-		console.log(`User: ${user} (${calculated})`)
-	}
+const checkUser = () => {
+if (user && state === 'requesting') {
+  console.log(`User: ${user} (${calculated})`)
 }
+}
+
 
 // Only allowed to change code above
 
---
+++ IWA_14/Challenge 1/Javascript/script.js
@@ -0,0 +1,14 @@
+const firstName = 'John';
+const age = 35;
+const hobby = 'Coding';
+
+const logTwice = (parameter) => {
+    console.log(messageHobby)
+    console.log(messageHobby)
+}
+
+function messageHobby () {
+  logTwice(`Hello, ${firstName} (${age}). I love ${hobby}!`)
+}
+
+messageHobby()
